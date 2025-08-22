9066e9648e95316d59f67cf1655e7a469a694908
Dhanush N <mrcoyote2075@gmail.com>
2025-08-22 14:19:13 +0530

    feat: Updated Vite Config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import chalk from 'chalk'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),
-    tailwindcss()
+    tailwindcss(),
+    {
+      name: 'custom-startup-message',
+      configureServer() {
+        setTimeout(() => {
+          console.log(" ");
+          console.log(chalk.greenBright('💻 Its From ' + chalk.white.bold("Dhanush!")))
+          console.log(chalk.gray('🎉 Special Thanks to The Team : '))
+          console.log(chalk.magentaBright.bold('✨ ' + chalk.underline('Praveen, Praveen Kumar, Harini, Shankar')))
+          console.log(" ");
+        }, 300)
+      }
+    }
+
   ],
   server: {
     port: 4154,
@@ -17,4 +31,5 @@ export default defineConfig({
       'cc444de18062.ngrok-free.app', // 👈 add your ngrok domain here
     ],
   },
+  
 })
